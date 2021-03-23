d84f5a2404da55ab1886786cee99899bbf134745
Liau Jian Jie <liaujianjie@gmail.com>
2021-03-23 14:33:15 +0800

    Instantiate a single express app per port

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app = express();
 
 const PORTS = [1111, 2222, 3333, 4444];
 
 PORTS.forEach((port) => {
+  const app = express();
   app.get("*", (req, res) => {
     res.status(200).json({
       port,
